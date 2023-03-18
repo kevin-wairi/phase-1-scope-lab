9e3081863b9ddcc674e2830c6883c537503cccad
kevin Wairi <kevinwairi@gmail.com>
2023-03-18 13:36:52 +0300

    completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+var customerName = 'bob';
+var bestCustomer;
+const LeastFavoriteCustomer='bob';
+
+
+function upperCaseCustomerName(){
+  return customerName = customerName.toUpperCase();
+}
+// the question says modify the customer Name var thats tricky bt its testing on scopes
+// upperCaseCustomerName();
+// console.log(customerName);
+// console.log(upperCaseCustomerName(customerName));
+// customerName=upperCaseCustomerName('bob');
+function setBestCustomer(){
+ return bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(){
+    return bestCustomer = 'maybe bob';
+}
+function changeLeastFavoriteCustomer(){
+    return LeastFavoriteCustomer = 'maybe bob';
+}
